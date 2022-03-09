3d6f1dbc08f3e30341e3d6761ca1f9fe3f2d18c9
Thomas Koman <99066789@mydavinci.nl>
2022-03-09 09:17:46 +0100

    clicker

--- clock.py
+++ clock.py
@@ -9,8 +9,6 @@ window.geometry("400x200")
 textbox = tkinter.Label(window, bg="black",  fg="dark Grey", font=("Helvetica",75))
 textbox.pack(side="top", pady=30)
 
-
-
 def clock():
     global textbox
     textbox.config(text=strftime("%H:%M:%S"))
